61d910460318e63bc89b43ffd46e9a34be43efa3
Matt Johnston <matt@ucc.asn.au>
2023-03-30 22:53:16 +0800

    Treat empty struct fields differently to a unit
    A Struct { } should have Some(Fields::Struct(vec![])) rather than None.
    
    This partially reverts cfb17c0cac7f19568d575117de3b218539e73ada (#45)

--- src/parse/body.rs
+++ src/parse/body.rs
@@ -25,19 +25,11 @@ impl StructBody {
         let fields = match group.delimiter() {
             Delimiter::Brace => {
                 let fields = UnnamedField::parse_with_name(&mut stream)?;
-                if fields.is_empty() {
-                    None
-                } else {
-                    Some(Fields::Struct(fields))
-                }
+                Some(Fields::Struct(fields))
             }
             Delimiter::Parenthesis => {
                 let fields = UnnamedField::parse(&mut stream)?;
-                if fields.is_empty() {
-                    None
-                } else {
-                    Some(Fields::Tuple(fields))
-                }
+                Some(Fields::Tuple(fields))
             }
             found => {
                 return Err(Error::InvalidRustSyntax {
@@ -117,14 +109,22 @@ fn test_struct_body_take() {
     assert_eq!(data_type, super::DataType::Struct);
     assert_eq!(ident, "Foo");
     let body = StructBody::take(stream).unwrap();
-    assert!(body.fields.is_none());
+    if let Some(Fields::Struct(v)) = body.fields {
+        assert!(v.len() == 0);
+    } else {
+        panic!("wrong fields {:?}", body.fields);
+    }
 
     let stream = &mut token_stream("struct Foo ()");
     let (data_type, ident) = super::DataType::take(stream).unwrap();
     assert_eq!(data_type, super::DataType::Struct);
     assert_eq!(ident, "Foo");
     let body = StructBody::take(stream).unwrap();
-    assert!(body.fields.is_none());
+    if let Some(Fields::Tuple(v)) = body.fields {
+        assert!(v.len() == 0);
+    } else {
+        panic!("wrong fields {:?}", body.fields);
+    }
 }
 
 /// The body of an enum
@@ -379,9 +379,6 @@ impl Fields {
                 })
                 .collect(),
         };
-        if cfg!(test) {
-            assert!(!result.is_empty());
-        }
         result
     }
 
