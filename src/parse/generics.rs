fcc951c0e164d83419dcf5921b5033f73af77836
Victor Koenders <victor.koenders@qrtech.se>
2023-03-30 10:04:31 +0200

    Added a test to validate that trailing commas on `where` conditions are okay

--- src/parse/generics.rs
+++ src/parse/generics.rs
@@ -617,3 +617,20 @@ fn test_generic_constraints_try_take() {
     let body = StructBody::take(stream).unwrap();
     assert_eq!(body.fields.len(), 0);
 }
+
+#[test]
+fn test_generic_constraints_trailing_comma() {
+    use crate::parse::{
+        Attribute, AttributeLocation, DataType, GenericConstraints, Generics, StructBody,
+        Visibility,
+    };
+    use crate::token_stream;
+    let source = &mut token_stream("pub struct MyStruct<T> where T: Clone, { }");
+
+    let attributes = Attribute::try_take(AttributeLocation::Container, source).unwrap();
+    let visibility = Visibility::try_take(source).unwrap();
+    let (datatype, name) = DataType::take(source).unwrap();
+    let generics = Generics::try_take(source).unwrap().unwrap();
+    let generic_constraints = GenericConstraints::try_take(source).unwrap().unwrap();
+    let body = StructBody::take(source).unwrap();
+}
--- test/src/main.rs
+++ test/src/main.rs
@@ -11,7 +11,16 @@ pub struct DefaultGeneric<T = ()> {
     pub t: T,
 }
 
+#[derive(virtue_test_derive::RetHi, Default)]
+pub struct MyStruct<T>
+where
+    T: Clone,
+{
+    pub b: Vec<T>,
+}
+
 fn main() {
     assert_eq!("hi", Foo::A.hi());
     assert_eq!("hi", Foo::B.hi());
+    assert_eq!("hi", MyStruct::<i32>::default().hi());
 }
